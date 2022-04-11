610a7728c356b32be453bf3cb4f939852735194f
Diadiva223 <diadiva223@gmail.com>
2022-04-11 01:49:13 -0700

    sord

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,21 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success/correct)
+ * Return: always 0 (success/correct)
  */
 
 int main(void)
 
-{	
-	char alphabet; 
+{
+char alphabet;
 
-	for (alphabet = 122 ; alphabet >= 97; alphabet--)
-	{
-		putchar(alphabet);
-	}
-
-	putchar('\n');
-
-	return (0);
-
+for alphabet = 122 ; alphabet >=97; alphabet --)
+{
+putchar(alphabet);
 }
+
+putchar('\n');
+
+return (0);
